bd2b13a8b59070b5c132a6accf71f19f6e20d768
Agustín <agustincalpedm@gmail.com>
2024-11-29 16:57:49 -0300

    Wrapping

--- bloc_de_notas.py
+++ bloc_de_notas.py
@@ -3,73 +3,116 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 
-guardado = True
+
+class App:
+
+    def __init__(self):
+        # ventana
+        self.ventana = tk.Tk()
+        self.ventana.title("Bloc de notas")
+        self.ventana.geometry("450x500")
+
+        # estados
+        self.guardado = True
+        self.wrap_text = False
+
+        # barra de menu
+        self.barra_menu = tk.Menu(self.ventana)
+        self.ventana.config(menu=self.barra_menu)
+
+        self.archivo_menu = tk.Menu(self.ventana, tearoff=0)
+        self.barra_menu.add_cascade(label="Archivo", menu=self.archivo_menu)
+
+        self.archivo_menu.add_command(
+            label="Abrir", command=self.abrir_archivo)
+        self.archivo_menu.add_command(label="Guardar")
+        self.archivo_menu.add_command(label="Cerrar")
+
+        self.edicion_menu = tk.Menu(self.ventana, tearoff=0)
+        self.barra_menu.add_cascade(label="Edición", menu=self.edicion_menu)
+
+        self.edicion_menu.add_command(
+            label="Ajustar texto", command=self.toggle_wrap)
+
+        # scrolltext
+        self.scroll_text = ScrolledText(
+            self.ventana, padx=5, pady=5, wrap="none")
+        self.scroll_text.pack(expand=True, fill="both")
+        self.scrollbar_horizontal = tk.Scrollbar(
+            self.ventana, orient=tk.HORIZONTAL)
+        self.scrollbar_horizontal.pack(side="bottom", fill="x")
+        self.scrollbar_horizontal.config(command=self.scroll_text.xview)
+        self.scroll_text.config(xscrollcommand=self.scrollbar_horizontal.set)
+
+        # menu contextual
+        self.menu_contextual = tk.Menu(self.ventana, tearoff=0)
+        self.menu_contextual.add_command(
+            label="Copiar", command=self.copiar_texto)
+        self.menu_contextual.add_command(
+            label="Cortar", command=self.cortar_texto)
+        self.menu_contextual.add_command(
+            label="Pegar", command=self.pegar_texto)
+
+        self.scroll_text.bind("<Button-3>", self.mostrar_menu_contextual)
+
+    def get_guardado(self):
+        """Comprueba si el archivo abierto esta guardado o no"""
+        return self.guardado
+
+    def set_guardado(self, value: bool):
+        """Cambia el valor de la variable guardado"""
+        self.guardado = value
+
+    def abrir_archivo(self):
+        """Lee un archivo y lo coloca en el scrolled text"""
+
+    def guardar_archivo(self):
+        """Guarda el archivo"""
+
+    def cerrar_archivo(self):
+        """Cierra el archivo"""
+
+    def mostrar_menu_contextual(self, event):
+        """Muestra el menu contextual"""
+        self.menu_contextual.tk_popup(event.x_root, event.y_root)
+
+    def copiar_texto(self):
+        """Copia el texto seleccionado"""
+        self.scroll_text.event_generate("<<Copy>>")
+
+    def cortar_texto(self):
+        """Corta el texto seleccionado"""
+        self.scroll_text.event_generate("<<Cut>>")
+
+    def pegar_texto(self):
+        """Pega el texto"""
+        self.scroll_text.event_generate("<<Paste>>")
+
+    def toggle_wrap(self):
+        """Alterna entre ajustar y desajustar el texto"""
+        self.scroll_text.config(wrap="none" if self.wrap_text else "word")
+        self.wrap_text = not self.wrap_text
+        self.edicion_menu.entryconfig(
+            0, label="Ajustar texto" if not self.wrap_text else "Desajustar texto")
+        self.toggle_scrollbar_horizontal()
+
+    def toggle_scrollbar_horizontal(self):
+        """Oculta o muestra la barra de desplazamiento horizontal"""
+        if self.wrap_text:
+            self.scrollbar_horizontal.forget()
+        else:
+            self.scrollbar_horizontal.pack(side="bottom", fill="x")
+
+    def iniciar(self):
+        """Inicia el programa"""
+        self.ventana.mainloop()
 
 
-def abrir_archivo():
-    """Lee un archivo y lo coloca en el scrolled text"""
+def main():
+    """Main del archivo"""
+    app = App()
+    app.iniciar()
 
 
-def guardar_archivo():
-    """Guarda el archivo"""
-    guardado = True
-
-
-def cerrar_archivo():
-    """Cierra el archivo"""
-
-
-def comprobar_guardado():
-    """Comprueba si el archivo abierto esta guardado o no"""
-    return guardado
-
-
-def mostrar_menu_contextual(event):
-    """Muestra el menu contextual"""
-    menu_contextual.tk_popup(event.x_root, event.y_root)
-
-
-def copiar_texto():
-    """Copia el texto seleccionado"""
-    scroll_text.event_generate("<<Copy>>")
-
-
-def cortar_texto():
-    """Corta el texto seleccionado"""
-    scroll_text.event_generate("<<Cut>>")
-
-
-def pegar_texto():
-    """Pega el texto"""
-    scroll_text.event_generate("<<Paste>>")
-
-
-# ------Ventana----------
-ventana = tk.Tk()
-ventana.title("Bloc de notas")
-ventana.geometry("450x500")
-
-# ----Barra de menu-------
-barra_menu = tk.Menu(ventana)
-ventana.config(menu=barra_menu)
-
-archivo_menu = tk.Menu(ventana, tearoff=0)
-barra_menu.add_cascade(label="Archivo", menu=archivo_menu)
-
-archivo_menu.add_command(label="Abrir", command=abrir_archivo)
-archivo_menu.add_command(label="Guardar")
-archivo_menu.add_command(label="Cerrar")
-
-# -----Scroll text-------
-scroll_text = ScrolledText(ventana, padx=5, pady=5, wrap="word")
-scroll_text.pack(expand=True, fill="both")
-
-# ----Contextual Menu------
-menu_contextual = tk.Menu(ventana, tearoff=0)
-menu_contextual.add_command(label="Copiar", command=copiar_texto)
-menu_contextual.add_command(label="Cortar", command=cortar_texto)
-menu_contextual.add_command(label="Pegar", command=pegar_texto)
-
-scroll_text.bind("<Button-3>", mostrar_menu_contextual)
-
-ventana.mainloop()
+if __name__ == "__main__":
+    main()
