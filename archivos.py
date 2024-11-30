15a1fadc7505bce2ec34d742cf940d3d4b5cd9da
Ezagu <agustincalpedm@gmail.com>
2024-11-30 02:17:12 -0300

    Control de Archivos

--- archivos.py
+++ archivos.py
@@ -1,13 +1,35 @@
+from tkinter import filedialog
+
 class Archivos:
 
     def __init__(self):
         self.archivo_actual_guardado: bool = None
 
-    def guardar_archivo(self):
-        pass
+    def guardar_archivo(self, contenido):
+        """Guarda el archivo
+        
+        Args:
+            contenido (str): Texto que se va a guardar
+        """
+        file_path = filedialog.asksaveasfilename(filetypes=[("Archivo de texto (.txt)", "*.txt"), ("Todos los archivos", "*.*")])
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(contenido)
+            file.close()
+            self.archivo_actual_guardado = True
 
-    def abrir_archivo(self):
-        pass
+    def abrir_archivo(self) -> str:
+        """Devuelve todo el contenido del archivo abierto"""
+        file_path = filedialog.askopenfilename(filetypes=[("Todos los archivos", "*.*"), ("Archivos de texto (.txt)", "*.txt")])
+        if file_path:
+            with open(file_path,"r", encoding="utf-8") as file:
+                contenido = file.read()
+                return contenido
 
     def cerrar_archivo(self):
         pass
+    
+    def is_archivo_actual_guardado(self):
+        return self.archivo_actual_guardado
+
+    def archivo_no_guardado(self, event):
+        self.archivo_actual_guardado = False
--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 """Bloc de notas"""
 
+from archivos import *
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 
@@ -14,6 +15,10 @@ class App:
 
         # estados
         self.wrap_text = False
+        self.archivo_guardado:bool = True
+
+        #archivos
+        self.path_actual = None
 
         # barra de menu
         self.barra_menu = tk.Menu(self.ventana)
@@ -22,9 +27,10 @@ class App:
         self.archivo_menu = tk.Menu(self.ventana, tearoff=0)
         self.barra_menu.add_cascade(label="Archivo", menu=self.archivo_menu)
 
-        self.archivo_menu.add_command(label="Abrir")
-        self.archivo_menu.add_command(label="Guardar")
-        self.archivo_menu.add_command(label="Cerrar")
+        self.archivo_menu.add_command(label="Abrir", command=self.abrir_archivo)
+        self.archivo_menu.add_command(label="Guardar", command=self.guardar_archivo)
+        self.archivo_menu.add_command(label="Guardar Como", command=self.guardar_archivo_como)
+        self.archivo_menu.add_command(label="Cerrar", command=self.cerrar_archivo)
 
         self.edicion_menu = tk.Menu(self.ventana, tearoff=0)
         self.barra_menu.add_cascade(label="Edición", menu=self.edicion_menu)
@@ -36,6 +42,9 @@ class App:
         self.scroll_text = ScrolledText(
             self.ventana, padx=5, pady=5, wrap="none")
         self.scroll_text.pack(expand=True, fill="both")
+        self.scroll_text.bind("<<Modified>>", self.guardado_false)
+
+        # scrollbar
         self.scrollbar_horizontal = tk.Scrollbar(
             self.ventana, orient=tk.HORIZONTAL)
         self.scrollbar_horizontal.pack(side="bottom", fill="x")
@@ -83,6 +92,75 @@ class App:
             self.scrollbar_horizontal.forget()
         else:
             self.scrollbar_horizontal.pack(side="bottom", fill="x")
+
+    def show_archivo_no_guardado(self):
+        ventana_emergente = tk.Toplevel(self.ventana)
+        ventana_emergente.title("Archivo sin guardar")
+        ventana_emergente.geometry("300x150")
+
+        label = tk.Label(ventana_emergente, text="Archivo no guardado")
+        label.pack()
+        boton_guardar = tk.Button(ventana_emergente,text="Guardar" ,command=self.guardar_archivo)
+        boton_no_guardar = tk.Button(ventana_emergente,text="No guardar" ,command=self.abrir_archivo)
+        boton_cancelar = tk.Button(ventana_emergente, text="Cancelar", command=lambda : cerrar_ventana(ventana_emergente))
+        boton_guardar.pack()
+        boton_no_guardar.pack()
+        boton_cancelar.pack()
+
+        def cerrar_ventana(ventana):
+            ventana.destroy()
+
+    def abrir_archivo(self):
+        print(self.archivo_guardado)
+        if not self.archivo_guardado:
+            self.show_archivo_no_guardado()
+        else:
+            file_path = filedialog.askopenfilename(filetypes=[("Todos los archivos", "*.*"), ("Archivos de texto (.txt)", "*.txt")])
+            if file_path:
+                with open(file_path,"r", encoding="utf-8") as file:
+                    contenido = file.read()
+                    self.scroll_text.delete(1.0,tk.END)
+                    self.scroll_text.insert(1.0,contenido)
+                    self.path_actual = file_path
+                    self.scroll_text.edit_modified(False)
+                    self.archivo_guardado = True          
+                    
+
+    def guardar_archivo(self):
+        """Guarda el archivo"""
+        if not self.path_actual:
+            self.guardar_archivo_como()
+        else:
+            contenido = self.scroll_text.get(1.0,tk.END)
+            with open(self.path_actual, "w", encoding="utf-8") as file:
+                file.write(contenido)
+                file.close()
+                self.archivo_guardado = True
+                self.scroll_text.edit_modified(False)
+
+    def guardar_archivo_como(self):
+        """Guarda el archivo"""
+        contenido = self.scroll_text.get(1.0,tk.END)
+        file_path = filedialog.asksaveasfilename(filetypes=[("Archivo de texto (.txt)", "*.txt"), ("Todos los archivos", "*.*")])
+        if file_path:
+            with open(file_path, "w", encoding="utf-8") as file:
+                file.write(contenido)
+                file.close()
+                self.path_actual = file_path
+                self.archivo_guardado = True
+                self.scroll_text.edit_modified(False)
+
+    def guardado_false(self, event):
+        """Indica que no se guardo el archivo"""
+        print("Texto modificado")
+        self.archivo_guardado = False
+
+    def cerrar_archivo(self):
+        """Cierra el archivo"""
+        if not self.archivo_guardado:
+            self.show_archivo_no_guardado()
+        else:
+            self.scroll_text.delete(1.0,tk.END)
 
     def iniciar(self):
         """Inicia el programa"""
